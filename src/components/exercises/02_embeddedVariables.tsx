905162187f84059a542d95135002b67894158461
OpinionatedHeron <leamoy@gmail.com>
2025-08-24 23:25:46 +0100

    Completing exercise 2
    Signed-off-by: OpinionatedHeron <leamoy@gmail.com>

--- src/components/exercises/02_embeddedVariables.tsx
+++ src/components/exercises/02_embeddedVariables.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
+const modules = [
+  { name: "DevOps", noLectures: 2, noPracticals: 2 },
+  { name: "Fullstack Development", noLectures: 3, noPracticals: 2 },
+];
+
+const header: string = "HDip Computer Science - Modules";
 
 const Demo: React.FC = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>{header}</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].noLectures}</td>
+            <td>{modules[0].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].noLectures}</td>
+            <td>{modules[1].noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
