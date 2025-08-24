9e0502dcb2fefcf6a44192979287197c448265e2
OpinionatedHeron <leamoy@gmail.com>
2025-08-25 00:58:38 +0100

    Completing exercise 4
    Signed-off-by: OpinionatedHeron <leamoy@gmail.com>

--- src/components/exercises/04_iteration.tsx
+++ src/components/exercises/04_iteration.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
+interface Module {
+  name: string;
+  noLectures: number;
+  noPracticals: number;
+}
 
-const Demo: React.FC = () => {
+interface CourseModulesProps {
+  title: string;
+  modules: Module[];
+}
+
+const Demo: React.FC<CourseModulesProps> = ({ title, modules }) => {
+  const list = modules.map((module, index) => (
+    <tr key={index}>
+      <td>{module.name}</td>
+      <td>{module.noLectures}</td>
+      <td>{module.noPracticals}</td>
+    </tr>
+  ));
+
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>{title}</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    </div>
   );
 };
 
