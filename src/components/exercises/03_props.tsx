7858424f662d0cc9745e24d26c0d1049b0f4529d
OpinionatedHeron <leamoy@gmail.com>
2025-08-24 23:43:43 +0100

    Completing exercise 3
    Signed-off-by: OpinionatedHeron <leamoy@gmail.com>

--- src/components/exercises/03_props.tsx
+++ src/components/exercises/03_props.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
+interface Module { name: string; noLectures: number; noPracticals: number; }
 
-const Demo: React.FC = () => {
+interface CourseModulesProps {
+  title: string;
+  modules: Module[];
+}
+
+const Demo: React.FC<CourseModulesProps>= ({title, modules}) => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>{title}</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].noLectures}</td>
+            <td>{modules[0].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].noLectures}</td>
+            <td>{modules[1].noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
