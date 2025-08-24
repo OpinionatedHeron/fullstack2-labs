a9d938cf1ffa51c3bce2aa540b1b3a96f0fd3429
OpinionatedHeron <leamoy@gmail.com>
2025-08-24 22:24:18 +0100

    Completing exercise 1
    Signed-off-by: OpinionatedHeron <leamoy@gmail.com>

--- src/components/exercises/01_staticComponent.tsx
+++ src/components/exercises/01_staticComponent.tsx
@@ -3,7 +3,30 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo: React.FC = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>HDip Computer Science - Modules</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>DevOps</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Fullstack Development</td>
+            <td>3</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
